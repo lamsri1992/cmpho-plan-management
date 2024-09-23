d5672fa8aadf5df1b7e04f728ad52d9cb083ac26
lamsri1992 <satiya3577@gmail.com>
2024-09-23 11:25:15 +0700

    update https

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -19,6 +19,7 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot(): void
     {
-        //
+        // HTTPS ALLOW
+        resolve(\Illuminate\Routing\UrlGenerator::class)->forceScheme('https');
     }
 }
